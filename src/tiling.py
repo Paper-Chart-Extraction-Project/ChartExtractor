9389b202300299bab0f8c1fbd2d26ff2ec3cfd0e
Folks <VCZ2AJ@uvahealth.org>
2024-05-23 13:28:59 -0400

    Created 'tiling.py' and added a docstring
    Created 'tiling.py'. This folder will contain an implementation of image tiling so that we can get better small object detection results.

---
+++ src/tiling.py
@@ -0,0 +1,4 @@
+"""The tiling.py module implements a function called 'tile_image' which slices images into smaller 
+pieces called 'tiles'. This is used to improve the accuracy and memory efficiency of object
+detection models that need to detect very small objects in high resolution images.
+"""
