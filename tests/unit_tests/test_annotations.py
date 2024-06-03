7bf98a4f2ad51f1a92a5de12bfb0f738d77233a3
Folks <VCZ2AJ@uvahealth.org>
2024-06-03 14:29:39 -0400

    Wrote validate_box_values tests.

--- tests/unit_tests/test_annotations.py
+++ tests/unit_tests/test_annotations.py
@@ -18,20 +18,25 @@ class TestBoundingBox:
 
     def test_validate_box_values_left_greater_than_right(self):
         """Tests the validate_box_values classmethod with invalid parameters (left > right)."""
-        pass
+        with pytest.raises(ValueError, match="left side greater than its right side"):
+            BoundingBox("Test", 1, 0, 0, 1)
 
     def test_validate_box_values_top_greater_than_bottom(self):
         """Tests the validate_box_values classmethod with invalid parameters (top > bottom)."""
-        pass
+        with pytest.raises(ValueError, match="top side greater than its bottom side"):
+            BoundingBox("Test", 0, 1, 1, 0)
 
     def test_validate_box_values_left_eq_right(self):
         """Tests the validate_box_values classmethod with degenerate rectangle parameters (left == right)."""
-        pass
+        with pytest.warns(UserWarning, match="left side equals its right side"):
+            BoundingBox("Test", 0, 0, 0, 1)
 
     def test_validate_box_values_top_eq_bottom(self):
         """Tests the validate_box_values classmethod with degenerate rectangle parameters (top == bottom)."""
-        pass
+        with pytest.warns(UserWarning, match="top side equals its bottom side"):
+            BoundingBox("Test", 0, 0, 1, 0)
 
     def test_validate_box_values_left_eq_right_top_eq_bottom(self):
         """Tests the validate_box_values classmethod with degernate rectangle parameters (left == right == top == bottom)."""
-        pass
+        with pytest.warns(UserWarning, match="box's parameters are equal"):
+            BoundingBox("Test", 0, 0, 0, 0)
