b25f6a4f680bf76145eeade292e5f8ce11edd7ea
Folks <VCZ2AJ@uvahealth.org>
2024-06-03 14:19:49 -0400

    Stubbed out init test.

--- tests/unit_tests/test_annotations.py
+++ tests/unit_tests/test_annotations.py
@@ -7,3 +7,11 @@ sys.path.insert(0, os.path.abspath(os.path.join("..", "..", "src")))
 
 import pytest
 import annotations
+
+
+class TestBoundingBox:
+    """Tests the BoundingBox class."""
+
+    def test_init(self):
+        """Tests the init function with valid parameters."""
+        pass
