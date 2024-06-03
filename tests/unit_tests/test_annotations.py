11319c5f897da1b107f9383f80d69379a60114f6
Folks <VCZ2AJ@uvahealth.org>
2024-06-03 14:21:35 -0400

    Stubbed out validate_box_values tests.

--- tests/unit_tests/test_annotations.py
+++ tests/unit_tests/test_annotations.py
@@ -15,3 +15,23 @@ class TestBoundingBox:
     def test_init(self):
         """Tests the init function with valid parameters."""
         BoundingBox("Test", 0, 0, 1, 1)
+
+    def test_validate_box_values_left_greater_than_right(self):
+        """Tests the validate_box_values classmethod with invalid parameters (left > right)."""
+        pass
+
+    def test_validate_box_values_top_greater_than_bottom(self):
+        """Tests the validate_box_values classmethod with invalid parameters (top > bottom)."""
+        pass
+
+    def test_validate_box_values_left_eq_right(self):
+        """Tests the validate_box_values classmethod with degenerate rectangle parameters (left == right)."""
+        pass
+
+    def test_validate_box_values_top_eq_bottom(self):
+        """Tests the validate_box_values classmethod with degenerate rectangle parameters (top == bottom)."""
+        pass
+
+    def test_validate_box_values_left_eq_right_top_eq_bottom(self):
+        """Tests the validate_box_values classmethod with degernate rectangle parameters (left == right == top == bottom)."""
+        pass
