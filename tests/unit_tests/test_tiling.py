22edcc2207a1a1cd25fa42651ce23475dff92081
Folks <VCZ2AJ@uvahealth.org>
2024-06-04 10:04:29 -0400

    Created a test fixture for creating a test image.

--- tests/unit_tests/test_tiling.py
+++ tests/unit_tests/test_tiling.py
@@ -6,4 +6,14 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join("..", "..", "src")))
 
 import pytest
+from PIL import Image
 import tiling
+
+
+@pytest.fixture(scope="class", autouse=True)
+def test_image(self):
+    """Creates a PIL Image for testing purposes."""
+    image = Image.new("L", (3, 3))
+    image_data = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    image.putdata(image_data)
+    return image
