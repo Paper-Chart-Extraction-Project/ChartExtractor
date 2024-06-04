f038c3463602efd5ed5b63ea643103f190fd6363
Folks <VCZ2AJ@uvahealth.org>
2024-06-04 10:23:53 -0400

    Wrote test_slice_height_too_large

--- tests/unit_tests/test_tiling.py
+++ tests/unit_tests/test_tiling.py
@@ -63,7 +63,16 @@ class TestValidateTileParameters:
 
     def test_slice_height_too_large(self, test_image):
         """Tests the validate_tile_parameters function when the slice height is greater than the image height."""
-        pass
+        slice_width, slice_height = 1, 4
+        horizontal_overlap_ratio, vertical_overlap_ratio = 0.5, 0.5
+        with pytest.raises(ValueError, match="slice_height must be"):
+            tiling.validate_tile_parameters(
+                test_image,
+                slice_width,
+                slice_height,
+                horizontal_overlap_ratio,
+                vertical_overlap_ratio,
+            )
 
     def test_horizontal_overlap_ratio_too_small(self, test_image):
         """Tests the validate_tile_parameters function when the horizontal overlap ratio is less than or equal to 0."""
