44c1f2c61da86353993dd6af0d5caf5ddde5d366
Folks <VCZ2AJ@uvahealth.org>
2024-06-05 10:44:48 -0400

    Stubbed out methods and wrote docstring summaries for them.

--- src/detection_reassembly.py
+++ src/detection_reassembly.py
@@ -1,1 +1,18 @@
 """ """
+
+from typing import List
+
+
+def non_maximum_suppression(detections: List):
+    """Filters overlapping detections to only those which have the highest confidence."""
+    pass
+
+
+def intersection_over_union(detection_1, detection_2):
+    """Divides two detection's area of intersection by their union area."""
+    pass
+
+
+def intersection_over_minimum(detection_1, detection_2):
+    """Divides two detection's area of intersection over the area of the smaller of the two detections."""
+    pass
