4c05f9d533875f3f5469d3694be024b18f614955
Folks <VCZ2AJ@uvahealth.org>
2024-06-05 12:52:52 -0400

    Stubbed out a fixture that generates Detections.

--- tests/unit_tests/test_detection_reassembly.py
+++ tests/unit_tests/test_detection_reassembly.py
@@ -7,3 +7,9 @@ sys.path.insert(0, os.path.abspath(os.path.join("..", "..", "src")))
 
 import pytest
 import detection_reassembly
+
+
+@pytest.fixture()
+def test_detections():
+    """Creates a short list of fake detections for testing purposes."""
+    pass
