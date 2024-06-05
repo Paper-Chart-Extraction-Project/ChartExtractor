5b7d00fa2eee4bf75e6fe4e7b33b621075ee6bac
Folks <VCZ2AJ@uvahealth.org>
2024-06-05 11:14:42 -0400

    Added type hints in detection_reassembly for the Detection object.

--- src/detection_reassembly.py
+++ src/detection_reassembly.py
@@ -1,18 +1,19 @@
 """ """
 
 from typing import List
+from detections import Detection
 
 
-def non_maximum_suppression(detections: List):
+def non_maximum_suppression(detections: List[Detection]):
     """Filters overlapping detections to only those which have the highest confidence."""
     pass
 
 
-def intersection_over_union(detection_1, detection_2):
+def intersection_over_union(detection_1: Detection, detection_2: Detection):
     """Divides two detection's area of intersection by their union area."""
     pass
 
 
-def intersection_over_minimum(detection_1, detection_2):
+def intersection_over_minimum(detection_1: Detection, detection_2: Detection):
     """Divides two detection's area of intersection over the area of the smaller of the two detections."""
     pass
