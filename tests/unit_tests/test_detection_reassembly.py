1833f0f96e6e175da0e487aa92d32e85e5760df2
Folks <VCZ2AJ@uvahealth.org>
2024-06-05 13:06:10 -0400

    Stubbed out the TestIntersectionOverUnion class
    And its test methods.

--- tests/unit_tests/test_detection_reassembly.py
+++ tests/unit_tests/test_detection_reassembly.py
@@ -18,4 +18,21 @@ def test_detections() -> List[Detection]:
     return [
         Detection(BoundingBox("Test", 0, 0, 1, 1), 1.0),
         Detection(BoundingBox("Test", 0.5, 0, 1, 1), 0.5),
+        Detection(BoundingBox("Test", 2, 2, 3, 3), 0.75),
     ]
+
+
+class TestIntersectionOverUnion:
+    """Tests the intersection_over_union function."""
+
+    def test_intersection_over_union(self, test_detections):
+        """Tests the intersection_over_union function with typical inputs."""
+        pass
+
+    def test_intersection_over_union_no_overlap(self, test_detections):
+        """Tests the intersection_over_union function with non-overlapping detections."""
+        pass
+
+    def test_intersection_over_union_total_overlap(self, test_detections):
+        """Tests the intersection_over_union function with totally overlapping detections."""
+        pass
