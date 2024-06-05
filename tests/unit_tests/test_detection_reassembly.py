863b564ad4cc8e819a7230b8a53ddc2138119f9b
Folks <VCZ2AJ@uvahealth.org>
2024-06-05 13:01:26 -0400

    Wrote function body for test_detections fixture.
    Also added type hinting for other fixtures in the unit_test folder.

--- tests/unit_tests/test_detection_reassembly.py
+++ tests/unit_tests/test_detection_reassembly.py
@@ -6,10 +6,16 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join("..", "..", "src")))
 
 import pytest
+from typing import List
+from annotations import BoundingBox
+from detections import Detection
 import detection_reassembly
 
 
 @pytest.fixture()
-def test_detections():
+def test_detections() -> List[Detection]:
     """Creates a short list of fake detections for testing purposes."""
-    pass
+    return [
+        Detection(BoundingBox("Test", 0, 0, 1, 1), 1.0),
+        Detection(BoundingBox("Test", 0.5, 0, 1, 1), 0.5),
+    ]
