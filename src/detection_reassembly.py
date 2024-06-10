e770ac164c51db0eb1315149c9ed659e6ba28a6d
Folks <VCZ2AJ@uvahealth.org>
2024-06-10 11:08:02 -0400

    Stubbed out and wrote docstrings for intersection_over_minimum.
    And other helper functions. Since IoU and IoM rely on the same set of helper functions, they will be extracted out into their own functions.

--- src/detection_reassembly.py
+++ src/detection_reassembly.py
@@ -10,8 +10,56 @@
     and a function that quantifies area of overlap (defaults to IOU).
 """
 
-from typing import Callable, List
+from typing import Callable, List, Tuple
 from detections import Detection
+
+
+def compute_area(box: Tuple[float, float, float, float]):
+    """Computes the area of a rectangle.
+
+    Args:
+        box (Tuple[float, float, float, float]):
+            A tuple of four floats that define the (left, top, right, bottom) of a rectangle.
+
+    Returns:
+        The area of the rectangle.
+    """
+    pass
+
+
+def compute_intersection_area(
+    box_1: Tuple[float, float, float, float], box_2: Tuple[float, float, float, float]
+) -> float:
+    """Computes the area of the intersection of two rectangle.
+
+    Args:
+        box_1 (Tuple[float, float, float, float]):
+            A tuple of four floats that define the (left, top, right, bottom) of the first rectangle.
+        box_2 (Tuple[float, float, float, float]):
+            A tuple of four floats that define the (left, top, right, bottom) of the second rectangle.
+
+    Returns:
+        The area of the intersection of the two rectangles box_1 and box_2.
+    """
+    pass
+
+
+def intersection_over_minimum(detection_1: Detection, detection_2: Detection) -> float:
+    """Calculates the Intersection over Minimum (IoM) between two detections.
+
+    This function calculates the area of overlap between the two bounding boxes of two
+    detection objects and divides it by the area of the smaller detection.
+
+    Args:
+        detection_1 (Detection):
+            A Detection object representing the first detection.
+        detection_2 (Detection):
+            A Detection object representing the second detection.
+
+    Returns:
+        A float value between 0.0 and 1.0 representing the IoM between the two detections.
+    """
+    pass
 
 
 def intersection_over_union(detection_1: Detection, detection_2: Detection) -> float:
@@ -21,9 +69,9 @@ def intersection_over_union(detection_1: Detection, detection_2: Detection) -> f
     detection objects and divides it by the total area covered by their bounding boxes.
 
     Args:
-        detection_1:
+        detection_1 (Detection):
             A Detection object representing the first detection.
-        detection_2:
+        detection_2 (Detection):
             A Detection object representing the second detection.
 
     Returns:
@@ -63,7 +111,7 @@ def non_maximum_suppression(
     Args:
         detections:
             A list of `Detection` objects representing the detections to be filtered.
-        threshold:
+        threshold (float):
             A float value between 0.0 and 1.0, representing the minimum IoU (Intersection
             over Union) threshold for discarding detections considered to overlap with
             a higher-confidence detection. (default: 0.5)
