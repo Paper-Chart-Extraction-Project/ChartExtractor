9ecf9ad7d339819dfc4add3935caa49fcaea3fd9
Folks <rfolks92@gmail.com>
2024-06-12 09:13:06 -0400

    Wrote tests for errors.
    These are not at all comprehensive.

--- tests/unit_tests/test_homography.py
+++ tests/unit_tests/test_homography.py
@@ -5,16 +5,31 @@ import os
 
 sys.path.insert(0, os.path.abspath(os.path.join("..", "..", "src")))
 
-from image_registration import homography
+import pytest
+from PIL import Image
+from image_registration.homography import homography_transform
+
+
+@pytest.fixture()
+def test_pil_image() -> Image.Image:
+    """Creates a PIL Image for testing purposes."""
+    image = Image.new("RGB", (3, 3))
+    return image
 
 
 class TestHomographyTransform:
     """Tests the homography_transform function."""
 
-    def test_not_enough_points():
+    def test_not_enough_points(self, test_pil_image):
         """Tests the homography_tranform function when there are not enough points."""
-        pass
+        with pytest.raises(ValueError, match="Must have 4"):
+            homography_transform(
+                test_pil_image, [(0, 1), (2, 3), (4, 5)], [(1, 2), (3, 4), (5, 6)]
+            )
 
-    def test_unequal_number_of_points():
+    def test_unequal_number_of_points(self):
         """Tests the homography_transform function when there are an unequal number of src and dst points."""
-        pass
+        with pytest.raises(ValueError, match="Source and destination points"):
+            homography_transform(
+                test_pil_image, [(0, 1), (2, 3)], [(1, 2), (3, 4), (5, 6)]
+            )
