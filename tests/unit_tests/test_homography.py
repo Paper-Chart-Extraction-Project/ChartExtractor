26dbe08e686c1f2b3a31e2c0caa5438c94afbf68
Folks <rfolks92@gmail.com>
2024-06-12 09:03:28 -0400

    Stubbed out test class.
    The only thing this class tests so far is whether the function throws errors, not its normal use.

--- tests/unit_tests/test_homography.py
+++ tests/unit_tests/test_homography.py
@@ -6,3 +6,15 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join("..", "..", "src")))
 
 from image_registration import homography
+
+
+class TestHomographyTransform:
+    """Tests the homography_transform function."""
+
+    def test_not_enough_points():
+        """Tests the homography_tranform function when there are not enough points."""
+        pass
+
+    def test_unequal_number_of_points():
+        """Tests the homography_transform function when there are an unequal number of src and dst points."""
+        pass
