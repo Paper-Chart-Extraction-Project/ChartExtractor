f4599f559f8d64457085941a075336e09f82e868
Folks <rfolks92@gmail.com>
2024-06-14 08:53:55 -0400

    Created the yolov8.py file and stubbed out the YOLOv8 class.

---
+++ src/object_detection_models/yolov8.py
@@ -0,0 +1,22 @@
+""" """
+
+from pathlib import Path
+from object_detection_model import ObjectDetectionModel
+
+
+class YOLOv8(ObjectDetectionModel):
+    """ """
+
+    def __init__(self):
+        """ """
+        pass
+
+    @staticmethod
+    def from_weights_path(self, weights_path:Path) -> "YOLOv8":
+        """ """
+        pass
+
+    @staticmethod
+    def from_model(self, model) -> "YOLOv8":
+        """ """
+        pass
