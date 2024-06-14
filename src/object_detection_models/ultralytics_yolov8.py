ab2a4a6b9949f513a72f6d481753836ada73006a
Folks <rfolks92@gmail.com>
2024-06-14 09:12:36 -0400

    Implemented initialization functions.
    These seem very redundant, but I am trying to futureproof as much as I can, and other models might need a lot more code to get up and running than the simple syntax that Ultralytics employs.

--- src/object_detection_models/ultralytics_yolov8.py
+++ src/object_detection_models/ultralytics_yolov8.py
@@ -1,7 +1,7 @@
 """ """
 
 from pathlib import Path
-from typing import List
+from typing import Callable, List
 from PIL import Image
 from ultralytics import YOLO
 from utilities.detections import Detection
@@ -11,19 +11,38 @@ from object_detection_model import ObjectDetectionModel
 class UltralyticsYOLOv8(ObjectDetectionModel):
     """ """
 
-    def __init__(self):
-        """ """
-        pass
+    def __init__(self, model):
+        """Initializes the UltralyticsYOLOv8 object.
+        
+        Args:
+            model:
+                The Ultralytics class for the YOLOv8 model.
+        """
+        self.model = model
 
     @staticmethod
     def from_weights_path(self, weights_path:Path) -> "UltralyticsYOLOv8":
-        """ """
-        pass
+        """Creates an UltralyticsYOLOv8 object from a path to the weights file.
+        
+        Args:
+            weights_path (Path):
+                A path leading to the model's weights.pt file.
+        
+        Returns (UltralyticsYOLOv8):
+            An UltralyticsYOLOv8 object.
+        """
+        model = YOLO(str(weights_path))
+        return UltralyticsYOLOv8.from_model(model)
 
     @staticmethod
     def from_model(self, model) -> "UltralyticsYOLOv8":
-        """ """
-        pass
+        """Creates an UltralyticsYOLOv8 object from the Ultralytics model object.
+        
+        Args:
+            model:
+                The Ultralytics class for the YOLOv8 model.
+        """
+        return UltralyticsYOLOv8(model)
 
     def __call__(self, Image: Image.Image) -> List[Detection]:
         """ """
