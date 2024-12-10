f7cc8831da849f56015f36a8d0e0cb7f0f893b26
RyanDoesMath <rfolks92@gmail.com>
2024-12-10 13:59:37 -0500

    Add docstring for compute_digit_distances_to_centroids

--- src/extraction/drug_codes.py
+++ src/extraction/drug_codes.py
@@ -75,6 +75,21 @@ def compute_digit_distances_to_centroids(
     im_width: int,
     im_height: int,
 ) -> Dict[str, int]:
+    """Computes the distances between the digit detections and the number box centroids.
+
+    Args:
+        number_detections (List[BoundingBox]):
+            Handwritten digit bounding boxes.
+        centroids (Dict[str, Tuple[float, float]]):
+            Tuples of floats that encode the centroid of a sample of single digit number boxes.
+        im_width (int):
+            The width of the image the detections were made on.
+        im_height (int):
+            The height of the image the detections were made on.
+
+    Returns:
+        A dictionary mapping the names of the centroids to the closest bounding box.
+    """
     euclidean_distance = lambda x1, y1, x2, y2: np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
     normalize_box_loc = lambda center: (center[0] / im_width, center[1] / im_height)
 
