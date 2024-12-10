ca89c9a12b755cd1164a9f49470c05fa4ddff67a
RyanDoesMath <rfolks92@gmail.com>
2024-12-10 13:42:54 -0500

    Add centroids constant.

--- src/extraction/drug_codes.py
+++ src/extraction/drug_codes.py
@@ -1,6 +1,7 @@
 """Provides functions for extracting drug codes."""
 
 # Built-in imports
+import json
 from pathlib import Path
 from typing import Dict, List, Tuple
 
@@ -8,8 +9,13 @@ from typing import Dict, List, Tuple
 from utilities.detections import Detection
 
 
-FILEPATH_TO_NUMBER_BOX_CENTROIDS: Path = Path("")
-NUMBER_BOX_CENTROIDS: Dict[str, Tuple[float, float]] = {}
+DATA_FILEPATH: Path = Path(__file__).parents[2] / "data"
+FILEPATH_TO_NUMBER_BOX_CENTROIDS: Path = (
+    DATA_FILEPATH / "centroids" / "intraop_single_digit_box_centroids.json"
+)
+NUMBER_BOX_CENTROIDS: Dict[str, Tuple[float, float]] = json.load(
+    open(FILEPATH_TO_NUMBER_BOX_CENTROIDS, "r")
+)
 
 
 def extract_drug_codes(
