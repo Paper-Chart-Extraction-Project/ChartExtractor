553c8de290101d1876aacad37f02a821da24b9d1
RyanDoesMath <rfolks92@gmail.com>
2024-12-10 13:51:08 -0500

    Wrote function body of extract_drug_codes.

--- src/extraction/drug_codes.py
+++ src/extraction/drug_codes.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from typing import Dict, List, Tuple
 
 # Internal imports
+from utilities.annotations import BoundingBox
 from utilities.detections import Detection
 
 
@@ -37,4 +38,28 @@ def extract_drug_codes(
         A dictionary mapping each line of the code section of the
         intraoperative record to the digits written on that line.
     """
-    pass
+    number_detections: List[BoundingBox] = [det.annotation for det in number_detections]
+    drug_centroids: Dict[str, Tuple[float, float]] = {
+        key: val for (key, val) in NUMBER_BOX_CENTROIDS.items() if "code_row" in key
+    }
+    drug_box_values: Dict[str, int] = compute_digit_distances_to_centroids(
+        number_detections, drug_centroids, im_width, im_height
+    )
+
+    drug_codes: Dict[str, str] = dict()
+    for ix in range(0, 11):
+        if all(
+            [
+                f"code_row{str(ix).zfill(2)}_col{jx}" in drug_box_values.keys()
+                for jx in range(0, 3)
+            ]
+        ):
+            drug_codes[f"drug_row_{str(ix).zfill(2)}"] = "".join(
+                [
+                    drug_box_values[f"code_row{str(ix).zfill(2)}_col{0}"].category,
+                    drug_box_values[f"code_row{str(ix).zfill(2)}_col{1}"].category,
+                    drug_box_values[f"code_row{str(ix).zfill(2)}_col{2}"].category,
+                ]
+            )
+
+    return drug_codes
