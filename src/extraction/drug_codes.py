ad99ae4b555b17d0584963f9bbf9dc8113b024e5
RyanDoesMath <rfolks92@gmail.com>
2024-12-10 13:35:22 -0500

    Stub out the extract_drug_codes function.

---
+++ src/extraction/drug_codes.py
@@ -0,0 +1,16 @@
+"""Provides functions for extracting drug codes."""
+
+# Built-in imports
+from typing import Dict, List
+
+# Internal imports
+from utilities.detections import Detection
+
+
+def extract_drug_codes(
+    number_detections: List[Detection],
+    im_width: int,
+    im_height: int,
+) -> Dict[str, int]:
+    """ """
+    pass
