284d5ba674047c9dff2659fe6dbbf86c32130ff4
RyanDoesMath <rfolks92@gmail.com>
2024-12-12 13:35:25 -0500

    Add some helper functions.

--- src/extraction/preoperative_postoperative_digit_boxes.py
+++ src/extraction/preoperative_postoperative_digit_boxes.py
@@ -19,3 +19,38 @@ FILEPATH_TO_NUMBER_BOX_CENTROIDS: Path = (
 NUMBER_BOX_CENTROIDS: Dict[str, Tuple[float, float]] = json.load(
     open(FILEPATH_TO_NUMBER_BOX_CENTROIDS, "r")
 )
+
+
+def get_relevant_boxes(
+    number_detections: List[Detection],
+    keyword: str,
+    im_width: int,
+    im_height: int,
+) -> Dict[str, BoundingBox]:
+    """Gets the relevant BoundingBoxes from the list of all detections.
+
+    Args:
+        `number_detections` (List[Detection]):
+            The handwritten numbers that have been detected.
+        `keyword` (str):
+            A word that appears in all the bounding boxes 'category' attributes.
+        `im_width` (int):
+            The width of the image the detections were made on.
+        `im_height` (int):
+            The height of the image the detections were made on.
+
+    Returns:
+        A dictionary mapping the names of the relevant digit boxes to the closest bounding boxes.
+    """
+    number_detections: List[BoundingBox] = [det.annotation for det in number_detections]
+    filtered_centroids: Dict[str, Tuple[float, float]] = {
+        key: val for (key, val) in NUMBER_BOX_CENTROIDS.items() if keyword in key
+    }
+    values: Dict[str, BoundingBox] = compute_digit_distances_to_centroids(
+        number_detections, filtered_centroids, im_width, im_height
+    )
+    return values
+
+
+def get_category_or_space(bb):
+    return bb.category if bb is not None else " "
