df3d376483a52f6a54205b406cf1390f7f30d93f
RyanDoesMath <rfolks92@gmail.com>
2024-12-16 13:17:37 -0500

    Stub out extract_physiological_indicators.

--- src/extraction/physiological_indicators.py
+++ src/extraction/physiological_indicators.py
@@ -23,3 +23,29 @@ PHYSIO_LANDMARK_NAMES: List[str] = [
     "urine_output",
     "blood_loss",
 ]
+
+
+def extract_physiological_indicators(
+    digit_detections: List[Detection],
+    legend_locations: Dict[str, Tuple[float, float]],
+    document_detections: List[Detection],
+    im_width: int,
+    im_height: int,
+) -> Dict[str, Dict[str, List[int]]]:
+    """Extracts all of the physiological indicator data from the number detections.
+
+    Args:
+        `number_detections` (List[Detection]):
+            The handwritten numbers that have been detected.
+        `legend_locations` (Dict[str, Tuple[float, float]]):
+            The location of timestamps and mmhg/bpm values on the legend.
+        `im_width` (int):
+            The width of the image the detections were made on.
+        `im_height` (int):
+            The height of the image the detections were made on.
+
+    Returns:
+        A dictionary mapping the name of the physiological indicator with a dictionary
+        that maps timestamps to values.
+    """
+    pass
