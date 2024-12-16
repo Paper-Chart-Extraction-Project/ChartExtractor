d7b2c0b4e8f6cc1b426243a54faffbb298a71cdf
Ryan Folks <rfolks92@gmail.com>
2024-12-16 17:22:01 -0500

    Add docstrings for functions in blood_pressure_and_heart_rate.py

--- src/extraction/blood_pressure_and_heart_rate.py
+++ src/extraction/blood_pressure_and_heart_rate.py
@@ -10,12 +10,34 @@ from utilities.detections import Detection
 
 
 def find_timestamp(time_legend: List[Cluster], keypoint_x: float) -> str:
-    """ """
+    """Given a keypoint on a blood pressure or heart rate detection, finds the timestamp.
+
+    Args:
+        `time_legend` (List[Cluster]):
+            The named clusters which form the timestamp legend that runs horizontally on the top
+            side of the blood pressure and heart rate section.
+        `keypoint_x` (float):
+            The x value of the keypoint.
+
+    Returns:
+        The label of the closest timestamp cluster.
+    """
     pass
 
 
 def find_value(value_legend: List[Cluster], keypoint_y: float) -> int:
-    """ """
+    """Given a keypoint on a blood pressure or heart rate detection, finds the in mmhg/bpm value.
+
+    Args:
+        `value_legend` (List[Cluster]):
+            The named clusters which form the mmhg/bpm legend that runs vertically on the left
+            side of the blood pressure and heart rate section.
+        `keypoint_y` (float):
+            The y value of the keypoint.
+
+    Returns:
+        The approximate value that the keypoint encodes in mmhg/bpm.
+    """
     pass
 
 
@@ -24,5 +46,18 @@ def extract_heart_rate_and_blood_pressure(
     time_clusters: List[Cluster],
     value_clusters: List[Cluster],
 ) -> Dict[str, Dict[str, str]]:
-    """ """
+    """Extracts the heart rate and blood pressure data from the detections.
+
+    Args:
+        `detections` (List[Detection]):
+            The keypoint detections of the systolic, diastolic, and heart rate markings.
+        `time_clusters` (List[Cluster]):
+            The clusters corresponding to the timestamps.
+        `value_clusters` (List[Cluster]):
+            The clusters corresponding to the mmhg and bpm values.
+
+    Returns:
+        A dictionary mapping each timestamp to the systolic, diastolic, and heart rate reading
+        that was recorded at that time.
+    """
     pass
