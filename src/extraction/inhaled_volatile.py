963dd5c0aa2748ce7501fffbb944873b708a3bcc
RyanDoesMath <rfolks92@gmail.com>
2024-12-17 13:27:15 -0500

    Stub out extract_inhaled_volatile.

--- src/extraction/inhaled_volatile.py
+++ src/extraction/inhaled_volatile.py
@@ -1,1 +1,12 @@
 """Extracts the inhaled volatile drug data."""
+
+# Built-in imports
+from typing import Dict
+
+# Internal imports
+from blood_pressure_and_heart_rate import find_timestamp, find_value
+
+
+def extract_inhaled_volatile() -> Dict[str, str]:
+    """ """
+    pass
