b4580f7686acb297e82fdb622f4c0cfb24f94872
RyanDoesMath <rfolks92@gmail.com>
2024-12-17 13:33:08 -0500

    Add docstring for extract_inhaled_volatile.

--- src/extraction/inhaled_volatile.py
+++ src/extraction/inhaled_volatile.py
@@ -1,12 +1,34 @@
 """Extracts the inhaled volatile drug data."""
 
 # Built-in imports
-from typing import Dict
+from typing import Dict, List, Tuple
 
 # Internal imports
-from blood_pressure_and_heart_rate import find_timestamp, find_value
+from utilities.detections import Detection
 
 
-def extract_inhaled_volatile() -> Dict[str, str]:
-    """ """
+def extract_inhaled_volatile(
+    digit_detections: List[Detection],
+    legend_locations: Dict[str, Tuple[float, float]],
+    document_detections: List[Detection],
+    im_width: int,
+    im_height: int,
+) -> Dict[str, str]:
+    """Extracts the inhaled volatile gas data from the number detections.
+
+    Args:
+        `digit_detections` (List[Detection]):
+            The handwritten numbers that have been detected.
+        `legend_locations` (Dict[str, Tuple[float, float]]):
+            The location of the timestamps and mmhg/bpm values on the legend.
+        `document_detections` (List[Detection]):
+            The location of document landmarks that have been detected.
+        `im_width` (int):
+            The width of the image the detections were made on.
+        `im_height` (int):
+            The height of the image the detections were made on.
+
+    Returns:
+        A dictionary mapping timestamps to inhaled volatile gas data.
+    """
     pass
