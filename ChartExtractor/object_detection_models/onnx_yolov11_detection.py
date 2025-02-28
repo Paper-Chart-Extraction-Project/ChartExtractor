5522cd700e4fab361c066ddf9f5b36ae0f0279cc
Ryan Folks <rfolks92@gmail.com>
2025-02-28 17:38:35 -0500

    Create onnx_yolov11_detection.py.

---
+++ ChartExtractor/object_detection_models/onnx_yolov11_detection.py
@@ -0,0 +1,18 @@
+"""This module implements the `OnnxYolov11Detection` wrapper class
+
+The `OnnxYolov11Detection` class, which inherits from the `ObjectDetectionModel` interface,
+provides a wrapper for the YOLOv11 object detection model using the onnx runtime.
+
+Key functionalities include:
+    - Provides a common interface for detections (via the __call__ method).
+    - Loading the YOLOv11 model from a weights file path.
+    - Preprocessing images and postprocessing detections.
+    - Performing object detection on an image using the YOLOv11 model.
+    - Converting the YOLOv11 model's output to a list of Detection objects.
+
+These `Detection` objects encapsulate details about detected objects, including bounding boxes,
+confidence scores, and potentially keypoints (if available in the model's output).
+
+This approach simplifies the integration and usage of YOLOv8 within this program, promoting code
+modularity and reusability.
+"""
