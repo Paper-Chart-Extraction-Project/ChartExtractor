220c5e3bbcad4b788b34d0e56c11325397566151
RyanDoesMath <rfolks92@gmail.com>
2025-03-01 13:14:14 -0500

    Make enough changes to make the file run.
    The code is still slow compared to what it should be (runs in 2 seconds, but the actual model runs in about 0.4 seconds). This is due to postprocessing being slow in python. One last thing to try is ultralytics yolov11, but loaded from an onnx model.

--- ChartExtractor/object_detection_models/onnx_yolov11_detection.py
+++ ChartExtractor/object_detection_models/onnx_yolov11_detection.py
@@ -19,18 +19,22 @@ modularity and reusability.
 
 # Built-in imports
 from pathlib import Path
-from typing import List
+from typing import Dict, List
 
 # External imports
+import cv2
 import numpy as np
 import onnxruntime as ort
 import yaml
 
 # Internal imports
 from ..object_detection_models.object_detection_model import ObjectDetectionModel
+from ..utilities.annotations import BoundingBox
 from ..utilities.detections import Detection
 from ..utilities.detection_reassembly import non_maximum_suppression
 
+
+class_num = 256
 NUM_HEADS: int = 3
 STRIDES: List[int] = [8, 16, 32]
 MAPSIZE: List[List[int]] = [[80, 80], [40, 40], [20, 20]]
@@ -38,8 +42,8 @@ MESHGRID: List = list()
 for index in range(NUM_HEADS):
     for ix in range(MAPSIZE[index][0]):
         for jx in range(MAPSIZE[index][1]):
-            MESHGRID.append(j+0.5)
-            MESHGRID.append(i+0.5)
+            MESHGRID.append(jx+0.5)
+            MESHGRID.append(ix+0.5)
 
 
 class OnnxYolov11Detection(ObjectDetectionModel):
@@ -74,25 +78,21 @@ class OnnxYolov11Detection(ObjectDetectionModel):
                 The image height that the model accepts.
                 Defaults to 640.
         """
-        self.model = ort.InferenceSession(self.model_weights_filepath)
+        self.model = ort.InferenceSession(model_weights_filepath)
         self.input_im_width = input_im_width
         self.input_im_height = input_im_height
         self.classes = OnnxYolov11Detection.load_classes(model_metadata_filepath)
         
+    def from_model(self):
+        pass
+    
+    def from_weights_path(self):
+        pass
+    
     @staticmethod
     def load_classes(model_metadata_filepath) -> Dict[int, str]:
         """ """
         pass
-
-    def sigmoid(self, x) -> int:
-        """Applies the sigmoid function to x.
-        
-        Args:
-            x: the input number.
-
-        Returns: 1/(1+e^-x)
-        """
-        return 1/(1+np.exp(-x))
 
     def __call__(
         self, 
@@ -114,7 +114,7 @@ class OnnxYolov11Detection(ObjectDetectionModel):
         if not isinstance(images, list):
             images = [images]
         detections: List[List[Detection]] = [
-            self.detect(im) for im in images
+            self.detect(im, confidence) for im in images
         ]
         return detections
 
@@ -140,7 +140,8 @@ class OnnxYolov11Detection(ObjectDetectionModel):
         image: np.array = image.transpose((2, 0, 1))
         image: np.array = np.expand_dims(image, axis=0)
         pred_results = self.model.run(None, {'data': image})
-        detections = self.postprocess_results(pred_results, im_width, im_height)
+        detections = self.postprocess_results(pred_results, im_width, im_height, confidence)
+        return detections
 
     def preprocess_image(
         self,
@@ -175,7 +176,7 @@ class OnnxYolov11Detection(ObjectDetectionModel):
         """Processes the raw results from the onnx model.
         
         I don't like this method. It was copy pasted from someone else's code and it is
-        very obtuse and difficult to understand.
+        very obtuse and difficult to understand. Not only that but it is horribly slow.
 
         Args:
             pred_results:
@@ -191,52 +192,64 @@ class OnnxYolov11Detection(ObjectDetectionModel):
         Returns:
             A list of Detection objects.
         """
-        results = list()
-        pred_results = [pred_results[i].reshape(-1) for i in range(len(out))]
-        scalar_w = image_width/self.input_im_width
-        scalar_h = image_height/self.input_im_height
-        grid_index = -2 # magic number...
+        def sigmoid(x) -> int:
+            """Applies the sigmoid function to x.
+            
+            Args:
+                x: the input number.
+
+            Returns: 1/(1+e^-x)
+            """
+            return 1/(1+np.exp(-x))
+        
+        pred_results = [1/(1+np.exp(-pr)) for pr in pred_results]
+        detections: List[Detection] = list()
+        pred_results = [pred_results[i].reshape(-1) for i in range(len(pred_results))]
+        scalar_w: float = image_width/self.input_im_width
+        scalar_h: float = image_height/self.input_im_height
+        grid_index: int = -2 # magic number...
 
         for index in range(NUM_HEADS):
-            regression = output[index * 2 + 0]
-            classification = output[index * 2 + 1]
+            regression = pred_results[index * 2 + 0]
+            classification = pred_results[index * 2 + 1]
 
             for h in range(MAPSIZE[index][0]):
                 for w in range(MAPSIZE[index][1]):
                     grid_index += 2
 
                     if class_num == 1:
-                        cls_max = self.sigmoid(cls[h*MAPSIZE[index][1]+w])
+                        cls_max = classification[h*MAPSIZE[index][1]+w]
                         cls_index: int = 0
                     else:
                         cls_max, cls_index = max(
                             [
-                                (self.sigmoid(cls[cl*MAPSIZE[index][1]+h*MAPSIZE[index][1]+w]), cl)
-                                for cl in range(class_num)]
+                                (classification[cl*MAPSIZE[index][1]+h*MAPSIZE[index][1]+w], cl)
+                                for cl in range(class_num)
+                            ]
                         )
 
-                    if cls_max > self.confidence:
+                    if cls_max > confidence:
                         regdfl = list()
                         for lc in range(4): #four???
                             locval: int = 0
-                            sfsum = sum(np.exp(
-                                reg[((lc*16)+df)*MAPSIZE[index][0]*MAPSIZE[index][1]+h*MAPSIZE[index][1]+w])
+                            sfsum = np.sum(np.exp(
+                                regression[((lc*16)+df)*MAPSIZE[index][0]*MAPSIZE[index][1]+h*MAPSIZE[index][1]+w])
                                 for df in range (16)
                             )
                             for df in range(16):
-                                sfval = np.exp(reg[((lc*16)+df) * MAPSIZE[index][0] * MAPSIZE[index][1] + h * MAPSIZE[index][1]+w]) / sfsum
+                                sfval = np.exp(regression[((lc*16)+df) * MAPSIZE[index][0] * MAPSIZE[index][1] + h * MAPSIZE[index][1]+w]) / sfsum
                                 locval += sfval * df
                             regdfl.append(locval)
 
-                        x1 = (MESHGRID[grid_index+0] - regdfl[0]) * strides[index]
-                        y1 = (MESHGRID[grid_index+1] - regdfl[1]) * strides[index]
-                        x2 = (MESHGRID[grid_index+0] - regdfl[2]) * strides[index]
-                        y2 = (MESHGRID[grid_index+1] - regdfl[3]) * strides[index]
+                        x1 = (MESHGRID[grid_index+0] - regdfl[0]) * STRIDES[index]
+                        y1 = (MESHGRID[grid_index+1] - regdfl[1]) * STRIDES[index]
+                        x2 = (MESHGRID[grid_index+0] + regdfl[2]) * STRIDES[index]
+                        y2 = (MESHGRID[grid_index+1] + regdfl[3]) * STRIDES[index]
 
                         xmin = max(0, x1*scalar_w)
                         ymin = max(0, y1*scalar_h)
-                        xmax = min(input_im_width, x2*scalar_w)
-                        ymax = min(input_im_height, y2*scalar_h)
+                        xmax = min(image_width, x2*scalar_w)
+                        ymax = min(image_height, y2*scalar_h)
                         
                         det = Detection(
                             BoundingBox(cls_index, xmin, ymin, xmax, ymax),
@@ -244,6 +257,6 @@ class OnnxYolov11Detection(ObjectDetectionModel):
                         )
                         detections.append(det)
         
-        detections: List[Detection] = non_maximum_suppresion(detections)
+        detections: List[Detection] = non_maximum_suppression(detections)
         return detections
 
