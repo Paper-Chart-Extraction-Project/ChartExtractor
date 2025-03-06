60e9367453155427c3d8503bb171d3e660c02af7
Ryan Folks <rfolks92@gmail.com>
2025-03-06 14:31:04 -0500

    Change ObjectDetectionModel from an interface to a protocol.
    ObjectDetectionModel was acting a bit too much like a parent class, so its two constructor methods were removed and the class was changed to a protocol which enforces that an implementer's object can be called to produce a list of Detection objects.

--- ChartExtractor/object_detection_models/object_detection_model.py
+++ ChartExtractor/object_detection_models/object_detection_model.py
@@ -1,64 +1,28 @@
-"""This module defines the `ObjectDetectionModel` interface, which serves as a base class
+"""This module defines the `ObjectDetectionModel` protocol, which serves as a structural subtype
 for any object detection model to be used with this program.
 
-The field of object detection is constantly evolving, with new and improved models
-emerging frequently. This interface facilitates the integration and testing of such models
-within the program. Instead of rewriting functionality for each new detector, developers can
-create subclasses that inherit from `ObjectDetectionModel`. As long as these subclasses
-override the `__call__` method to handle object detection on a PIL image and return a list
-of `Detection` objects, existing code remains compatible.
+This protocol will help us define core functionality between objects that adhere to this
+protocol without relying on inheritance.
+
+Implementers of this protocol should explicitly subclass it to enable static checking.
 
 This approach promotes modularity, flexibility, and future-proofing of the program.
 """
 
 # Built-in Imports
-from abc import ABC, abstractmethod
 from pathlib import Path
 from PIL import Image
-from typing import List
+from typing import List, Protocol
 
 # Internal Imports
 from ..utilities.detections import Detection
 
 
-class ObjectDetectionModel(ABC):
+class ObjectDetectionModel(Protocol):
     """Abstract base class for object detection models.
 
     This class defines the interface that all concrete object detection models must adhere to.
     """
-
-    @staticmethod
-    @abstractmethod
-    def from_weights_path(model_weights_path: Path) -> "ObjectDetectionModel":
-        """Initializes the ObjectDetectionModel from a path to its weights.
-
-        Args:
-            model_path (Path):
-                The path to the model's weights file.
-
-        Raises:
-            FileNotFoundError:
-                If the filepath does not lead to a file.
-
-        Returns:
-            ObjectDetectionModel:
-                An instance of the concrete subclass initialized from the weights.
-        """
-        pass
-
-    @staticmethod
-    @abstractmethod
-    def from_model(model) -> "ObjectDetectionModel":
-        """Initializes the ObjectDetectionModel from a model object.
-
-        Args:
-            model:
-                An object from another package that performs object detection.
-
-        Returns:
-            ObjectDetectionModel: An instance of the concrete subclass initialized from the model.
-        """
-        pass
 
     @abstractmethod
     def __call__(self, image: Image.Image) -> List[Detection]:
