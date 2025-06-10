c1a01717811cc85feb2dd7022e56a537dc53f610
Ryan Folks <rfolks92@gmail.com>
2025-06-10 08:18:13 -0400

    add tests for Detection.to_dict().

--- tests/unit_tests/test_detections.py
+++ tests/unit_tests/test_detections.py
@@ -52,7 +52,47 @@ class TestDetection:
             confidence=0.8,
         )
         assert Detection.from_dict(det_dict, Keypoint) == true_det
+    
+    def test_from_dict_bounding_box(self):
+        """Tests the to_dict method with a bounding box."""
+        det = Detection(
+            annotation=BoundingBox("Test", 0, 2, 1, 3),
+            confidence=0.8,
+        )
+        true_dict = {
+            "annotation": {
+                "left": 0,
+                "right": 1,
+                "top": 2,
+                "bottom": 3,
+                "category": "Test",
+            },
+            "confidence": 0.8,
+        }
+        assert det.to_dict() == true_dict
 
-    def test_to_dict(self):
-        """Tests the to_dict method."""
-        pass
+    def test_to_dict_keypoint(self):
+        """Tests the to_dict method with a keypoint."""
+        det = Detection(
+            annotation=Keypoint(Point(0.5, 2.25), BoundingBox("Test", 0, 2, 1, 3)),
+            confidence=0.8,
+        )
+        true_dict = {
+            "annotation": {
+                "bounding_box": {
+                    "left": 0,
+                    "right": 1,
+                    "top": 2,
+                    "bottom": 3,
+                    "category": "Test",
+                },
+                "keypoint": {
+                    "x": 0.5,
+                    "y": 2.25,
+                },
+            },
+            "confidence": 0.8,
+        }
+        
+        assert det.to_dict() == true_dict
+
