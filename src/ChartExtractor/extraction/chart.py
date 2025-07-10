4c00c8b7d8aed39ddd125850d1ed5126fa65f36c
Ryan Folks <rfolks92@gmail.com>
2025-07-10 14:04:48 -0400

    Add the Chart dataclass.

---
+++ src/ChartExtractor/extraction/chart.py
@@ -0,0 +1,13 @@
+"""A file that contains the Chart dataclass."""
+
+from dataclasses import dataclass
+from typing import Dict
+
+
+@dataclass
+class Chart:
+    """A dataclass that defines a chart.
+    
+    Serializes and deserializes to JSON.
+    """
+    pass
