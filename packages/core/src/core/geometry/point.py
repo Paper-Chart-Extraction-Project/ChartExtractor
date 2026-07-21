8cd9b0bc1525c62bd9d268b76a07a5354c56015a
RyanDoesMath <rfolks92@gmail.com>
2026-07-21 11:47:13 -0400

    Create the Point class.

--- packages/core/src/core/geometry/point.py
+++ packages/core/src/core/geometry/point.py
@@ -1,1 +1,21 @@
 """Contains the Point class."""
+
+from pydantic import BaseModel, ConfigDict
+
+
+class Point(BaseModel):
+    """A two-dimensional point in space.
+
+    An immutable point in 2d.
+
+    Attributes:
+        x (float):
+            The x value of the point.
+        y (float):
+            The y value of the point.
+    """
+
+    model_config = ConfigDict(frozen=True)
+
+    x: float
+    y: float
