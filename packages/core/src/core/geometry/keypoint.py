05baad3a9c9978938dd31c1960bfe4bff8aaba43
RyanDoesMath <rfolks92@gmail.com>
2026-07-23 07:59:50 -0400

    Create the Keypoint class.
    This class is just a Point along with a classification and a visibility status.

--- packages/core/src/core/geometry/keypoint.py
+++ packages/core/src/core/geometry/keypoint.py
@@ -1,4 +1,7 @@
 """Contains the Keypoint class."""
+
+from core.geometry import Point
+from pydantic import ConfigDict, Field
 from enum import StrEnum
 
 
@@ -9,3 +12,18 @@ class VisibilityStatus(StrEnum):
     OCCLUDED = "occluded"
     MISSING = "missing"
 
+
+class Keypoint(Point):
+    """A point with a classification.
+
+    Attributes:
+        category (str):
+            The keypoint's category. Cannot be empty.
+        visibility (VisibilityStatus):
+            The visibility of the keypoint.
+    """
+
+    model_config = ConfigDict(frozen=True)
+
+    category: str = Field(min_length=1)
+    visibility: VisibilityStatus
