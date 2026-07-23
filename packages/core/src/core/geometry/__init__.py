f9b13bf6de6ded36248f1c51bbd349f0914498ba
RyanDoesMath <rfolks92@gmail.com>
2026-07-23 09:26:13 -0400

    Update __init__.py to expose BoundingBoxWithKeypoints at the subpackage level.

--- packages/core/src/core/geometry/__init__.py
+++ packages/core/src/core/geometry/__init__.py
@@ -2,10 +2,12 @@ from .point import Point
 from .rectangle import Rectangle
 from .bounding_box import BoundingBox
 from .keypoint import Keypoint, VisibilityStatus
+from .bounding_box_with_keypoints import BoundingBoxWithKeypoints
 from typing import List
 
 __all__: List[str] = [
     "BoundingBox",
+    "BoundingBoxWithKeypoints",
     "Keypoint",
     "Point",
     "Rectangle",
