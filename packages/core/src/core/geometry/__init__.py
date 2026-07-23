75908c284742ca4c6525da142f5bd086539ef5a5
RyanDoesMath <rfolks92@gmail.com>
2026-07-23 08:24:42 -0400

    Update __init__.py to expose new objects at the subpackage level.
    Had to reorder the imports as well. If Point isn't first it leads to a circular import.

--- packages/core/src/core/geometry/__init__.py
+++ packages/core/src/core/geometry/__init__.py
@@ -1,6 +1,13 @@
+from .point import Point
+from .rectangle import Rectangle
 from .bounding_box import BoundingBox
 from .keypoint import Keypoint, VisibilityStatus
-from .point import Point
 from typing import List
 
-__all__: List[str] = ["BoundingBox", "Keypoint", "Point", "VisibilityStatus"]
+__all__: List[str] = [
+    "BoundingBox",
+    "Keypoint",
+    "Point",
+    "Rectangle",
+    "VisibilityStatus",
+]
