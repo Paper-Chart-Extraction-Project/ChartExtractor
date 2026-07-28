f96cdea3535f25e0ac0594197ca1f71d86d571ac
Ryan Folks <rfolks92@gmail.com>
2026-07-28 09:23:54 -0400

    Create the detections package.
    Add the bounding_box_detection.py file with a definition of the BoundingBoxDetection class.
    
    Add the keypoint_detection.py file with a definition of the KeypointDetection class.
    
    Add the bounding_box_with_keypoints_detection.py file with a definition of the BoundingBoxWithKeypoints class.
    
    Expose classes at the subpackage level.

---
+++ packages/core/src/core/detections/__init__.py
@@ -0,0 +1,10 @@
+from .bounding_box_detection import BoundingBoxDetection
+from .keypoint_detection import KeypointDetection
+from .bounding_box_with_keypoints_detection import BoundingBoxWithKeypointsDetection
+
+
+__all__: list[str] = [
+    "BoundingBoxDetection",
+    "KeypointDetection",
+    "BoundingBoxWithKeypointsDetection",
+]
