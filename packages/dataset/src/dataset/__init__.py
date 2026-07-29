d1d56e5751fb21446ebadc6b01d0a6f55fd1436a
RyanDoesMath <rfolks92@gmail.com>
2026-07-29 13:10:12 -0400

    Create VisionDataset
    Expose VisionDataset at the package level.

--- packages/dataset/src/dataset/__init__.py
+++ packages/dataset/src/dataset/__init__.py
@@ -1,4 +1,5 @@
 from .annotated_image import AnnotatedImage
 from .types import ImageId
+from .vision_dataset import VisionDataset
 
-__all__: list[str] = ["AnnotatedImage", "ImageId"]
+__all__: list[str] = ["AnnotatedImage", "ImageId", "VisionDataset"]
